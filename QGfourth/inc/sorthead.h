728c1ec243f373276e4ff24dbfad13a0c06d3eea
Light-DG <1454582179@qq.com>
2020-04-19 19:04:35 +0800

    add QGfourth-2

---
+++ QGfourth/inc/sorthead.h
@@ -0,0 +1,17 @@
+
+#ifndef SORTHEAD_H_INCLUDED
+#define SORTHEAD_H_INCLUDED
+void menu03(); 
+void insertsort(int arry[],int n);
+void myprint(int num[],int n);
+void merge(int arry[],int left,int middle,int right);
+void mergesort(int arry[],int left,int right);
+void quickSort(int num[],int left,int right);
+void countsort(int arry[],int n,int max);
+void arryClear(int arry[],int n);
+void radixsort(int arry[],int maxterm,int n);
+int getmaxterm(int max);
+void arryClear(int arry[],int n);
+void menu04();
+
+#endif 
